657f5367401606afc3b96c7f00cdc70cde2d5ec8
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-05-03 01:35:37 +0300

    a few changes

--- app/src/main/java/com/example/camerashooter/ConnectionActivity.java
+++ app/src/main/java/com/example/camerashooter/ConnectionActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -27,7 +26,6 @@ public class ConnectionActivity extends AppCompatActivity implements View.OnClic
         sendButton.setOnClickListener(this);
 
     }
-
 
     @Override
     public void onClick(View v) {
