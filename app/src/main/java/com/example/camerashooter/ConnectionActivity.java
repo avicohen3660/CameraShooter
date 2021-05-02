d5059f27c22b870a438241f754de68ff4442e09e
USER <avi>
2021-05-03 02:14:57 +0300

    basic socket

--- app/src/main/java/com/example/camerashooter/ConnectionActivity.java
+++ app/src/main/java/com/example/camerashooter/ConnectionActivity.java
@@ -3,18 +3,23 @@ package com.example.camerashooter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class ConnectionActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final int port = 5555;
+    public static final int port = 55555;
     EditText ipEditText;
     Button sendButton;
+    Socket socket;
+    DataOutputStream dos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +32,32 @@ public class ConnectionActivity extends AppCompatActivity implements View.OnClic
 
     }
 
+
     @Override
     public void onClick(View v) {
         String ip = ipEditText.getText().toString();
         if (isLegalIp(ip)) {
-            try {
-                Socket socket = new Socket(ip, port);
-                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                dos.writeUTF("hello");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (socket == null) {
+                            socket = new Socket("10.0.2.2", port);
+                            dos = new DataOutputStream(socket.getOutputStream());
+                        }
+                        Log.v("AVI","after");
+                        dos.writeUTF(ipEditText.getText().toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.v("AVI",e.getMessage());
+                    }
+                }
+            }).start();
+
         }
     }
 
-    public boolean isLegalIp(String ip){
+    public boolean isLegalIp(String ip) {
 
         return true;
     }
