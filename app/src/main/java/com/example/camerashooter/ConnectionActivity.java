2c89907836a5d5f7685574cb38a7ac1190ed155f
USER <avi>
2021-05-03 01:13:58 +0300

    test

---
+++ app/src/main/java/com/example/camerashooter/ConnectionActivity.java
@@ -0,0 +1,20 @@
+package com.example.camerashooter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class ConnectionActivity extends AppCompatActivity {
+
+    EditText ipEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+
+//        ipEditText
+
+    }
+}
