609d355b9b93b4501bf886b49b2ea861556a9c1c
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-05-03 14:21:13 +0300

    Cameracanvas created

--- app/src/main/java/com/example/camerashooter/CameraActivity.java
+++ app/src/main/java/com/example/camerashooter/CameraActivity.java
@@ -1,20 +1,25 @@
 package com.example.camerashooter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
-import android.graphics.Camera;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class CameraActivity extends AppCompatActivity {
 
+   private static final int CAMERA_REQUEST = 1888;
+   private static final int MY_CAMERA_PERMISSION_CODE = 100;
    FrameLayout frameLayout;
    Drawable aim;
-   CameraManager camera;
 
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
@@ -22,13 +27,42 @@ public class CameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-
         aim = getResources().getDrawable(R.drawable.aim);
         frameLayout = findViewById(R.id.container);
 
         frameLayout.setForegroundGravity(FrameLayout.TEXT_ALIGNMENT_CENTER);
         frameLayout.setForeground(aim);
 
-
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+        }else{
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+
+        //todo get dot in photo
+        }
+    }
+
 }
--- app/src/main/java/com/example/camerashooter/ConnectionActivity.java
+++ app/src/main/java/com/example/camerashooter/ConnectionActivity.java
@@ -2,6 +2,8 @@ package com.example.camerashooter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,32 +31,32 @@ public class ConnectionActivity extends AppCompatActivity implements View.OnClic
         ipEditText = findViewById(R.id.get_ip);
         sendButton = findViewById(R.id.send);
         sendButton.setOnClickListener(this);
-
     }
 
 
     @Override
     public void onClick(View v) {
-        String ip = ipEditText.getText().toString();
-        if (isLegalIp(ip)) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        if (socket == null) {
-                            socket = new Socket("10.0.2.2", port);
-                            dos = new DataOutputStream(socket.getOutputStream());
-                        }
-                        Log.v("AVI","after");
-                        dos.writeUTF(ipEditText.getText().toString());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        Log.v("AVI",e.getMessage());
-                    }
-                }
-            }).start();
-
-        }
+////        String ip = ipEditText.getText().toString();
+////        if (isLegalIp(ip)) {
+////            new Thread(new Runnable() {
+////                @Override
+////                public void run() {
+////                    try {
+////                        if (socket == null) {
+////                            socket = new Socket("10.0.2.2", port);
+////                            dos = new DataOutputStream(socket.getOutputStream());
+////                        }
+////                        Log.v("AVI","after");
+////                        dos.writeUTF(ipEditText.getText().toString());
+////                    } catch (Exception e) {
+////                        e.printStackTrace();
+////                        Log.v("AVI",e.getMessage());
+////                    }
+////                }
+////            }).start();
+//
+//        }
+        startActivity(new Intent(this, CameraActivity.class));
     }
 
     public boolean isLegalIp(String ip) {
