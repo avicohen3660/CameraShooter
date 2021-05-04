2725d7478b6acbd74d93f96e17d60f2a875bbcfd
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-05-04 17:07:54 +0300

    update FragmentGuns FragmentMagazines FragmentScopes

--- app/src/main/java/shop_fragments/FragmentMagazines.java
+++ app/src/main/java/shop_fragments/FragmentMagazines.java
@@ -1,5 +1,6 @@
 package shop_fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +16,11 @@ import java.util.Objects;
 
 public class FragmentMagazines extends Fragment {
 
-    @Nullable
-    @Override
+    @Override  @Nullable  @SuppressLint("SetTextI18n")
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_magazines, container, false);
+        View view =inflater.inflate(R.layout.fragment_magazines, container, false);
+        TextView shopType = view.findViewById(R.id.tv_shop_type);
+        shopType.setText("Magazine Shop");
+        return view;
     }
 }
--- app/src/main/java/shop_fragments/FragmentScopes.java
+++ app/src/main/java/shop_fragments/FragmentScopes.java
@@ -1,19 +1,24 @@
 package shop_fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.camerashooter.R;
 
 public class FragmentScopes extends Fragment {
 
-        @Nullable
-        @Override
+        @Override @Nullable @SuppressLint("SetTextI18n")
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_scopes, container, false);
+            View view =inflater.inflate(R.layout.fragment_scopes, container, false);
+            TextView shopType = view.findViewById(R.id.tv_shop_type);
+            shopType.setText("Scope Shop");
+            return view;
         }
     }
 
