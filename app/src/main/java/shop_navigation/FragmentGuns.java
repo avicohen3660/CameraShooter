daf6375a71e8fb48095fec3458a95c613ca98aa5
USER <avi>
2021-05-04 16:56:48 +0300

    magazine icon

--- app/src/main/java/shop_navigation/FragmentGuns.java
+++ app/src/main/java/shop_navigation/FragmentGuns.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.camerashooter.R;
@@ -13,7 +15,9 @@ public class FragmentGuns extends Fragment {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_guns, container, false);
+            View view = inflater.inflate(R.layout.fragment_guns, container, false);
+            ((TextView)view.findViewById(R.id.tv)).setText("dgfv");
+            return view;
 
         }
 
